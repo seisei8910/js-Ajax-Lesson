9f03c0f803b53aba7754c92780e6d3b4aeb59df6
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-17 22:20:29 +0000

    天気情報をリクエストする処理を追記

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+const API_KEY = "64167083b92a4686b4779e445997778e"
+
 $(function(){
   $('#btn').on('click', function(){
-    
+    $.ajax({
+      url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
+      dataType : 'jsonp',
+    })
   });
 });
