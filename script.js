5d93f289ddc3cfc80b0c48e2216a7e1591a616b7
SeiseiUtaka <185202718+seisei8910@users.noreply.github.com>
2025-03-17 22:23:39 +0000

    成功時と失敗時の処理を追記

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ $(function(){
     $.ajax({
       url: "https://api.openweathermap.org/data/2.5/weather?q=" + $('#cityname').val() + "&units=metric&appid=" + API_KEY,
       dataType : 'jsonp',
-    })
+    }).done(function (data){}{
+    }).fail(function (data){}{
+    });
   });
 });
